a2a6a61873d546a379b9954dd99ff122e565b4a0
fahriyafi <fahriyafi71@gmail.com>
2026-02-09 21:16:11 +0800

    Add files via upload

---
+++ pertambahanduaangka/src/App/pertambahan2Angka.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package App;
+import frame.frameApp;
+/**
+ *
+ * @author User
+ */
+public class pertambahan2Angka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frameApp frame = new frameApp();
+        frame.setVisible(true);
+    }
+    
+}
